c265fc0eaa86577ed9fb5d34a3b5f26f6df6ad01
Maxime <maxime.doury@gmail.com>
2023-01-05 20:03:03 +0100

    Improve theme config
    - Enable contrasting popup background
    - Enable buffer borders

--- lua/lazyvim/plugins/colorscheme.lua
+++ lua/lazyvim/plugins/colorscheme.lua
@@ -18,6 +18,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = true
       vim.cmd([[colorscheme nord]])
     end,
   },
